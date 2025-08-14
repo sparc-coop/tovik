f04ac4ebd2dcc7aebdb24db10d76ea543af82f38
Joseph Young <joseph@josephayoung.com>
2025-08-14 10:55:10 -0500

    Add Tovik Wordpress plugin

---
+++ Tovik.WordPress/tovik.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name: Tovik
+ * Description: Your site, multilingual in minutes.
+ * Version: 1.0
+ * Author: Sparc Cooperative
+ * Author URI: https://sparc.coop/
+ * License: GPL2
+ * Text Domain: Tovik
+ */
+
+
+function tovik_scripts() {
+    wp_enqueue_script_module('tovik', 'https://tovik.app/tovik.js');
+}
+
+add_action( 'wp_enqueue_scripts', 'tovik_scripts' );
+
+?>
