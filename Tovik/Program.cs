cef461d7059986acb7e483ccf3ddc0865812a87c
Joseph Young <joseph@josephayoung.com>
2026-03-11 08:56:24 -0500

    Merge from main

--- Tovik/Program.cs
+++ Tovik/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.DataProtection;
 using Sparc.Blossom.Data;
-using Sparc.Blossom.Engine;
-using Sparc.Blossom.Platforms.Server;
 using Tovik;
 using Tovik.Domains;
 using Tovik.Translation;
@@ -10,8 +8,8 @@ using Tovik.Translation;
 var builder = BlossomApplication.CreateBuilder<Html>(args);
 
 var dbName = builder.Configuration["Tovik"]!.Contains("localhost") ? "sparc-dev" : "sparc";
+builder.AddSparcEngine(builder.Configuration["SparcEngine"]);
 builder.Services.AddCosmos<TovikContext>(builder.Configuration.GetConnectionString("Cosmos")!, dbName, ServiceLifetime.Scoped);
-builder.Services.AddBlossomEngine(builder.Configuration["SparcEngine"]);
 builder.Services.AddControllers();
 builder.Services.AddDataProtection()
     .SetApplicationName("Tovik")
